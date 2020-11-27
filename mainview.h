98f5cee10cc9d407e4902e0e3a991aa0cbccd121
Hindrik Stegenga <Hindrik1997@hotmail.com>
2020-11-27 14:32:00 +0100

    Initial setup with provided code.

---
+++ mainview.h
@@ -0,0 +1,64 @@
+#ifndef MAINVIEW_H
+#define MAINVIEW_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_4_1_Core>
+#include <QOpenGLDebugLogger>
+
+#include <QOpenGLShaderProgram>
+
+#include <QMouseEvent>
+
+#include "meshrenderer.h"
+#include "mesh.h"
+
+class MainView : public QOpenGLWidget, protected QOpenGLFunctions_4_1_Core {
+
+    Q_OBJECT
+
+public:
+    MainView(QWidget *Parent = 0);
+    ~MainView();
+
+    void setSubdivisionLevel(int level);
+    void updateMatrices();
+    void updateUniforms();
+    void updateBuffers(Mesh& currentMesh);
+
+protected:
+    void initializeGL();
+    void resizeGL(int newWidth, int newHeight);
+    void paintGL();
+
+    void mouseMoveEvent(QMouseEvent* Event);
+    void mousePressEvent(QMouseEvent* event);
+    void wheelEvent(QWheelEvent* event);
+    void keyPressEvent(QKeyEvent* event);
+
+private:
+    QOpenGLDebugLogger debugLogger;
+
+    QVector2D toNormalizedScreenCoordinates(int x, int y);
+
+    void createShaderPrograms();
+    void createBuffers();
+
+    //for zoom
+    float scale;
+    //for handling rotation
+    QVector3D oldVec;
+    QQuaternion rotationQuaternion;
+    bool dragging;
+
+    MeshRenderer mr;
+
+    Settings settings;
+
+    //we make mainwindow a friend so it can access settings
+    friend class MainWindow;
+private slots:
+    void onMessageLogged( QOpenGLDebugMessage Message );
+
+};
+
+#endif // MAINVIEW_H
---
+++ meshrenderer.cpp
@@ -0,0 +1,101 @@
+#include "meshrenderer.h"
+
+MeshRenderer::MeshRenderer()
+{
+    meshIBOSize = 0;
+}
+
+MeshRenderer::~MeshRenderer() {
+    gl->glDeleteVertexArrays(1, &vao);
+
+    gl->glDeleteBuffers(1, &meshCoordsBO);
+    gl->glDeleteBuffers(1, &meshNormalsBO);
+    gl->glDeleteBuffers(1, &meshIndexBO);
+}
+
+void MeshRenderer::init(QOpenGLFunctions_4_1_Core* f, Settings* s) {
+    gl = f;
+    settings = s;
+
+    initShaders();
+    initBuffers();
+}
+
+void MeshRenderer::initShaders() {
+    shaderProg.create();
+    shaderProg.addShaderFromSourceFile(QOpenGLShader::Vertex, ":/shaders/vertshader.glsl");
+    shaderProg.addShaderFromSourceFile(QOpenGLShader::Fragment, ":/shaders/fragshader.glsl");
+    shaderProg.link();
+
+    uniModelViewMatrix = gl->glGetUniformLocation(shaderProg.programId(), "modelviewmatrix");
+    uniProjectionMatrix = gl->glGetUniformLocation(shaderProg.programId(), "projectionmatrix");
+    uniNormalMatrix = gl->glGetUniformLocation(shaderProg.programId(), "normalmatrix");
+}
+
+void MeshRenderer::initBuffers() {
+
+    gl->glGenVertexArrays(1, &vao);
+    gl->glBindVertexArray(vao);
+
+    gl->glGenBuffers(1, &meshCoordsBO);
+    gl->glBindBuffer(GL_ARRAY_BUFFER, meshCoordsBO);
+    gl->glEnableVertexAttribArray(0);
+    gl->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    gl->glGenBuffers(1, &meshNormalsBO);
+    gl->glBindBuffer(GL_ARRAY_BUFFER, meshNormalsBO);
+    gl->glEnableVertexAttribArray(1);
+    gl->glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    gl->glGenBuffers(1, &meshIndexBO);
+    gl->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshIndexBO);
+
+    gl->glBindVertexArray(0);
+
+}
+
+void MeshRenderer::updateBuffers(Mesh& m) {
+    //gather attributes for current mesh
+    m.extractAttributes();
+    QVector<QVector3D>& vertexCoords = m.getVertexCoords();
+    QVector<QVector3D>& vertexNormals = m.getVertexNorms();
+    QVector<unsigned int>& polyIndices = m.getPolyIndices();
+
+    gl->glBindBuffer(GL_ARRAY_BUFFER, meshCoordsBO);
+    gl->glBufferData(GL_ARRAY_BUFFER, sizeof(QVector3D)*vertexCoords.size(), vertexCoords.data(), GL_STATIC_DRAW);
+
+    gl->glBindBuffer(GL_ARRAY_BUFFER, meshNormalsBO);
+    gl->glBufferData(GL_ARRAY_BUFFER, sizeof(QVector3D)*vertexNormals.size(), vertexNormals.data(), GL_STATIC_DRAW);
+
+    gl->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshIndexBO);
+    gl->glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*polyIndices.size(), polyIndices.data(), GL_STATIC_DRAW);
+
+
+    meshIBOSize = polyIndices.size();
+}
+
+void MeshRenderer::updateUniforms() {
+    gl->glUniformMatrix4fv(uniModelViewMatrix, 1, false, settings->modelViewMatrix.data());
+    gl->glUniformMatrix4fv(uniProjectionMatrix, 1, false, settings->projectionMatrix.data());
+    gl->glUniformMatrix3fv(uniNormalMatrix, 1, false, settings->normalMatrix.data());
+}
+
+void MeshRenderer::draw() {
+    gl->glClearColor(0.0, 0.0, 0.0, 1.0);
+    gl->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    shaderProg.bind();
+
+    if (settings->uniformUpdateRequired) {
+        updateUniforms();
+        settings->uniformUpdateRequired = false;
+    }
+
+    gl->glBindVertexArray(vao);
+
+    gl->glDrawElements(GL_TRIANGLES, meshIBOSize, GL_UNSIGNED_INT, 0);
+
+    gl->glBindVertexArray(0);
+
+    shaderProg.release();
+}
